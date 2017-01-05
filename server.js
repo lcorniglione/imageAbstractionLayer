c477c634d8cc05ab7c757e75084029f1b5464058
Luciano Corniglione <luciano.corniglione.minig@intel.com>
2017-01-05 15:56:53 +0000

    deploy

--- server.js
+++ server.js
@@ -1,1 +1,37 @@
-var express = require("express")
+var express = require("express");
+var req = require("request");
+
+var app = express();
+
+app.get('/api/imagesearch/:query', function(request, response) {
+    var query = request.params.query;
+    var url = 'https://www.googleapis.com/customsearch/v1?q=' + query + '&searchType=image&cx=016394767303026535170:tiofy6wflbk&key=AIzaSyDEKgzuEk7zh0a6nsRYtJ1EEDQn3KwHT3g';
+    var size = request.query.offset;
+    req(url, function(error, res, body) {
+        if (!error && response.statusCode == 200) {
+            var history = {term : query, when : Date.now()};
+            var bod = JSON.parse(body);
+            var list = makeList(bod);
+            response.send(list);
+        }
+        else {
+            response.send(error);
+        }
+    })
+
+});
+
+function makeList(body) {
+    var list = [];
+    for (var i = 0; i < body.items.length; i++) {
+        var obj = new Object();
+        obj.url = body.items[i].link;
+        obj.snippet = body.items[i].title;
+        obj.thumbnail = body.items[i].image.thumbnailLink;
+        obj.context = body.items[i].image.contextLink;
+        list.push(obj);
+    }
+    return list;
+}
+
+app.listen(process.env.PORT || 5000);
